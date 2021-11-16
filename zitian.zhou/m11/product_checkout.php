fc7b4e1e2cb98069900ff1ae35f4ef021977b931
Zitian Zhou <eimit@s-MacBook-Pro.local>
2021-11-16 13:29:14 -0800

    m11

---
+++ zitian.zhou/m11/product_checkout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Product Cart</title>
+	<? include "parts/meta.php"; ?>
+</head>
+<body>
+	<? include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+
+			<h2>Product Checkout</h2>
+			<a href="./">Cancel</a>
+			<div>Fill in Your Information</div>
+			<div><a href="product_confirmation.php">Pay</a></div>
+		</div>
+	</div>
+	
+</body>
+</html>
